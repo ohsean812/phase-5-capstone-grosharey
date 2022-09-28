68999b1e969df28ed42adf52c2727029ede57cf1
Sean Oh <ohsean812@gmail.com>
2022-09-27 23:22:47 -0400

    added images in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,38 @@ user3 = User.create(username: "username3", password: "password")
 #     Grocery.create(name: Faker::Food.dish, price: rand(10..20), quantity: Faker::Measurement.weight, store: "Costco", date: "Faker::Date.between(from: '2022-09-21', to: '2022-09-25')", owner: User.pluck(:username).sample)
 # end
 
+grocery1= Grocery.create(name: "Toothpaste",
+                        price: 10,
+                        quantity: "3 ea.",
+                        store: "Costco",
+                        date: "2022-09-21",
+                        owner: User.pluck(:username).sample
+                        )
+    grocery1.image.attach(io: File.open(Rails.root.join('app/assets/cat.jpeg')),
+                        filename: 'cat.jpeg')
+
+grocery2= Grocery.create(name: "Ketchup",
+                        price: 5,
+                        quantity: "10 oz * 2 ea.",
+                        store: "Sams Club",
+                        date: "2022-09-22",
+                        owner: User.pluck(:username).sample
+                        )
+    grocery2.image.attach(io: File.open(Rails.root.join('app/assets/dog.webp')),
+                        filename: 'dog.webp')
+
+grocery3= Grocery.create(name: "External Hard Drive",
+                        price: 100,
+                        quantity: "5 TB",
+                        store: "Microcenter",
+                        date: "2022-09-23",
+                        owner: User.pluck(:username).sample
+                        )
+    grocery3.image.attach(io: File.open(Rails.root.join('app/assets/samplepic.png')),
+                        filename: 'samplepic.png')
+
+
+
 20.times do
     Comment.create(content: Faker::Company.bs, user_id: rand(1..3), grocery_id: rand(1..8))
 end
